ea669cd00656a7c76bbcb8cb3cf51ea559b56452
DPazienza <d.pazienza1@studenti.uniba.it>
2025-01-15 11:39:36 +0100

    utlime modifiche

--- chatbot.py
+++ chatbot.py
@@ -38,20 +38,19 @@ class EmotionsGPTChatbot:
             emotion_scores = self.emotion_classifier.classify_emotion(user_input)
             # print(emotion_scores)
 
-            # # Explain predictions using LIME
-            # lime_explanation = self.emotion_classifier.explain_predictions_lime(user_input)
-            # print("\nLime Explanation for Predictions:")
-            # for feature, weight in lime_explanation.as_list():
-            #     print(f"Feature: {feature}, Weight: {weight}")
+            # Explain predictions using LIME
+            lime_explanation = self.emotion_classifier.explain_predictions_lime(user_input)
+            print("\nLime Explanation for Predictions:")
+            for feature, weight in lime_explanation.as_list():
+                print(f"Feature: {feature}, Weight: {weight}")
 
-            # # Explain predictions using SHAP
-            # shap_explanation = self.emotion_classifier.explain_predictions_shap(user_input)
-            # print("\nSHAP Explanation for Predictions:")
-            # shap_values = shap_explanation[0].values
-            # shap_values = shap_explanation[0].values
-            # features = shap_explanation.data[0]
-            # for feature, value in zip(features, shap_values):
-            #     print(f"Feature: {feature}, Weight: {value}")
+            # Explain predictions using SHAP
+            shap_explanation = self.emotion_classifier.explain_predictions_shap(user_input)
+            print("\nSHAP Explanation for Predictions:")
+            shap_values = shap_explanation[0].values
+            features = shap_explanation.data[0]
+            for feature, value in zip(features, shap_values):
+                print(f"Feature: {feature}, Weight: {value}")
 
             # Generate response
             response =self.chat_model.generate_response(user_input, emotion_scores)
